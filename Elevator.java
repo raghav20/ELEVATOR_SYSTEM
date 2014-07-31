dd85acd092f57e3c1d4ec54d69cc5c9b32cbe8d8
Prashant <prash.raaghav@gmail.com>
2014-07-31 16:58:29 -0400

    Create Elevator.java
    Represents a single Elevator

---
+++ Elevator.java
@@ -0,0 +1,51 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Elevator  {
+  private Integer currentFloor;
+  private Queue<Integer> goal;
+
+  public Elevator(Integer currentFloor, Queue<Integer> destinationFloors) {
+    this.currentFloor = currentFloor;
+    this.goal = new LinkedList<Integer>();
+  }
+
+  public int nextGoal(){
+    return this.goal.peek();
+  }
+
+  public int currentFloor(){
+    return this.currentFloor;
+  }
+
+  public void visitGoal(){
+    this.goal.remove();
+  }
+  public void addNewGoal(Integer g) {
+    this.goal.add(g);
+  }
+
+  public void moveUp() {
+    currentFloor++;
+  }
+
+  public void moveDown() {
+    currentFloor--;
+  }
+
+  public ElevatorDirection direction() {
+    if (goal.size() > 0){
+      if (currentFloor < goal.peek()){
+        return ElevatorDirection.ELEVATOR_UP;
+      } else if (currentFloor > goal.peek()) {
+        return ElevatorDirection.ELEVATOR_DOWN;
+      }
+    }
+    return ElevatorDirection.ELEVATOR_HOLD;
+  }
+
+  public ElevatorStatus status() {
+    return (goal.size() > 0)?ElevatorStatus.ELEVATOR_OCCUPIED:ElevatorStatus.ELEVATOR_EMPTY;
+  }
+
+}
